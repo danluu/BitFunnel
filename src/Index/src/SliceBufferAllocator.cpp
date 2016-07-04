71ec2bc0d505ea84f5b469facd19a7f1aae36a0f
Dan Luu <danluu@gmail.com>
2016-07-03 22:27:56 -0700

    WIP SliceBufferAllocator.

---
+++ src/Index/src/SliceBufferAllocator.cpp
@@ -0,0 +1,35 @@
+#include <inttypes.h>
+
+#include "BitFunnel/Utilities/Factories.h"
+#include "LoggerInterfaces/Logging.h"
+#include "SliceBufferAllocator.h"
+
+namespace BitFunnel
+{
+    SliceBufferAllocator::SliceBufferAllocator(size_t blockSize, size_t blockCount)
+        : m_blockAllocator(Factories::CreateBlockAllocator(blockSize, blockCount))
+    {
+    }
+
+
+    void* SliceBufferAllocator::Allocate(size_t byteSize)
+    {
+        // TODO: should this be more strict and allow only exactly sized blocks?
+        LogAssertB(m_blockAllocator->GetBlockSize() <= byteSize,
+                   "Allocate byteSize < block size.");
+
+        return m_blockAllocator->AllocateBlock();
+    }
+
+
+    void SliceBufferAllocator::Release(void* buffer)
+    {
+        m_blockAllocator->ReleaseBlock(reinterpret_cast<uint64_t*>(buffer));
+    }
+
+
+    size_t SliceBufferAllocator::GetSliceBufferSize() const
+    {
+        return m_blockAllocator->GetBlockSize();
+    }
+}
